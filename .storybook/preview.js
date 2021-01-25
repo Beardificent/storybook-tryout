c99b74fa366a9617f7db392c2bc75643608c4108
Beardificent <cis.magito@gmail.com>
2021-01-25 16:33:23 +0100

    installed and imported Chakra.ui to see difference between buttons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {addDecorator} from "@storybook/react";
 import Center from "../src/components/Center/Center";
+import {ThemeProvider, theme, CSSReset} from "@chakra-ui/react";
 
-addDecorator(story => <Center>{story()}</Center>);
+addDecorator(story => <ThemeProvider theme={theme}>
+    <CSSReset/>
+    {story()}
+</ThemeProvider>);
