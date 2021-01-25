7f5dc0880cfa55dd6d3f8740546ab794eb07fb45
Beardificent <cis.magito@gmail.com>
2021-01-25 14:29:19 +0100

    Removed single <Center> decorator and added decorators to default export, wrapping around all the stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -5,9 +5,10 @@ import Center from '../Center/Center';
 export default {
     title: 'Form/Button',
     component: Button,
+    decorators: [story => <Center>{story()}</Center>],
 }
 //center comes from Center.js and Center.css.
-export const Primary = () => <Center><Button variant="primary">Primary</Button></Center>
-export const Secondary = () => <Center><Button variant="secondary">Secondary</Button></Center>
+export const Primary = () => <Button variant="primary">Primary</Button>
+export const Secondary = () => <Button variant="secondary">Secondary</Button>
 export const Success = () => <Button variant="success">Success</Button>
 export const Danger = () => <Button variant="danger">Danger</Button>
